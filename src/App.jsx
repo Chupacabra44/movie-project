0a54adf56171c2342cb25ed9b0d9a6c4b648ad03
Sanja Daic <sanja.daic86@gmail.com>
2026-01-29 13:36:27 +0100

    adding search component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
+import { useState } from "react";
+import Search from "./components/Search";
+
 const App = () => {
+  const [search, setSearch] = useState("");
+
   return (
     <main>
       <div className="pattern" />
       <div className="wrapper">
         <header>
-          <img src="/public/hero-img.png" alt="Hero Banner" />
+          <img src="/hero-img.png" alt="Hero Banner" />
           <h1>
             Find <span className="text-gradient">Movies</span> You'll Enjoy
             Without the Hassle
           </h1>
         </header>
-        <p>Search</p>
+        <Search search={search} setSearch={setSearch} />
       </div>
     </main>
   );
---
+++ src/components/Search.jsx
@@ -0,0 +1,17 @@
+const Search = ({ search, setSearch }) => {
+  return (
+    <div className="search">
+      <div>
+        <img src="/search.png" alt="search" />
+        <input
+          type="text"
+          placeholder="Search through thousands of movies"
+          value={search}
+          onChange={(event) => setSearch(event.target.value)}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Search;
