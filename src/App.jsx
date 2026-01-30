0c4708df1ceaf85664487c8c81c155f27c4b0f4d
Sanja Daic <sanja.daic86@gmail.com>
2026-01-30 10:02:24 +0100

    adding useDebounce from react-use

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import Search from "./components/Search";
 import Spinner from "./components/Spinner";
 import MovieCard from "./components/MovieCard";
+import { useDebounce } from "react-use";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -18,6 +19,10 @@ const App = () => {
   const [errorMessage, setErrorMessage] = useState("");
   const [movieList, setMovieList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [debouncedSearch, setDebouncedSearch] = useState("");
+
+  // Debounce the search input to limit API calls
+  useDebounce(() => setDebouncedSearch(search), 500, [search]);
 
   const fetchMovies = async (query = "") => {
     setIsLoading(true);
@@ -48,8 +53,8 @@ const App = () => {
   };
 
   useEffect(() => {
-    fetchMovies(search);
-  }, [search]);
+    fetchMovies(debouncedSearch);
+  }, [debouncedSearch]);
 
   return (
     <main>
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -7,6 +7,7 @@ const MovieCard = ({ movie }) => {
   return (
     <div className="movie-card">
       <img
+        className="movie-poster"
         src={
           poster_path
             ? `https://image.tmdb.org/t/p/w500/${poster_path}`
